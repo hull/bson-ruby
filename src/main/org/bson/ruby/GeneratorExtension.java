7fcc0f84612416c147df85300e004215fa6d25b9
Mathieu Ravaux <mathieu.ravaux@gmail.com>
2019-11-29 15:01:37 +0100

    Fix for RUBY-1172: bson-ruby's org.bson.ByteBuf clashes with Java driver

--- src/main/org/bson/ruby/GeneratorExtension.java
+++ src/main/org/bson/ruby/GeneratorExtension.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.bson;
+package org.bson.ruby;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
--- src/main/org/bson/ruby/MachineId.java
+++ src/main/org/bson/ruby/MachineId.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.bson;
+package org.bson.ruby;
 
 import java.lang.management.ManagementFactory;
 import java.net.NetworkInterface;
